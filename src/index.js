21f1dffc35bc526b485d619e128e7ba106bcba02
Prasannajit Tripathy <prasannajit.tripathy@tesco.com>
2020-09-02 05:44:31 +0530

    Updated code read appkey,secretkey from config[locale]

--- src/index.js
+++ src/index.js
@@ -1,17 +1,42 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import SeasonDisplay from './SeasonDisplay';
+import Spinner from './Spinner';
+// const App = () => {
+//     window.navigator.geolocation.getCurrentPosition((position)=>{
+//         console.log("position is ",position);
+//     },(err)=>{
+//         console.log(err);
+//     });
+//     return (<div>Hello World</div>);
+// };
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+class App extends React.Component {
+    // constructor(props) {
+    //     super(props);
+    //     this.state = { lat: null, errorMessage: '' }
+    // }
+    state = { lat: null, errorMessage: '' }
+    componentDidMount() {
+        window.navigator.geolocation.getCurrentPosition((position) => {
+            console.log("position is ", position);
+            this.setState({ lat: position.coords.latitude })
+        }, (err) => {
+            this.setState({ errorMessage: err.message })
+        });
+    }
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+    renderContent() {
+        if (this.state.lat) {
+            return <SeasonDisplay lat={this.state.lat} />;
+        }
+        if (this.state.errorMessage) {
+            return <div>ERROR: {this.state.errorMessage}</div>;
+        }
+        return <div><Spinner message="Please accept location request" /></div>;
+    }
+    render() {
+        return (<div>{this.renderContent()}</div>)
+    }
+}
+ReactDOM.render(<App />, document.querySelector('#root'));
